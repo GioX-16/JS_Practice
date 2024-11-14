fc0864486a7da8ffcc93dd8c51641a0c2418f9c3
Geovanny Sandino <sandinowork16@gmail.com>
2024-11-14 14:16:28 -0600

    Terminamos los tipos basicos de JS

--- 01-Tipos/09-Argumentos.js
+++ 01-Tipos/09-Argumentos.js
@@ -1,12 +1,15 @@
 
 // cuando Referencias los valores DENTRO de una funcion se le llama PARAMETRO
 function suma(a) {
+    console.log(arguments);
     return a + 2;
 }
 
 // Los valores que se les pasan a las funciones se llaman ARGUMENTOS // 
-let resultado = suma(6, 5);
+let resultado = suma(6, 5, 7, 8, 9);
 console.log(resultado);
 console.log(typeof suma);
 
 // las funciones pueden resivir multiples parametros y argumentos
+
+
