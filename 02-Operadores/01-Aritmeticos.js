c913b54025b8c09dfb974848cbf6416b572ee98b
Geovanny Sandino <sandinowork16@gmail.com>
2024-11-21 09:16:42 -0600

    Agregamos un nuevo archivo

---
+++ 02-Operadores/01-Aritmeticos.js
@@ -0,0 +1,23 @@
+let a = 5;
+let b = 7;
+
+/* console.log(a + b, 'Suma'); 
+console.log(a - b, 'Retsa'); 
+console.log(a * b, 'Multiplicacion'); 
+console.log(a / b, 'Division'); 
+console.log(a % b, 'Modulo'); 
+console.log(a ** b, 'Potencia');  */
+
+
+/* Incrementar */
+
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+/* Decrementar */
+
+console.log(--a);
+console.log(a--);
+console.log(a);
+
