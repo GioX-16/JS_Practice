54c4cb0fee41b1ad91834a21ca28345c6658fe27
Geovanny Sandino <sandinowork16@gmail.com>
2024-12-27 17:01:06 -0600

    we add 04-operator_logical

---
+++ 02-Operadores/04-Logicos.js
@@ -0,0 +1,17 @@
+// AND, OR , NOT
+
+// Operador AND (&&) - Devuelve true si ambos operandos son true 
+
+let mayor = false;
+let suscrito = true;
+
+console.log( 'operador and', mayor && suscrito); // false   
+
+// Operador OR (||) - Devuelve true si al menos uno de los operandos es true
+
+console.log( 'operador or', mayor || suscrito); // true
+
+// Operador NOT (!) - Devuelve true si el operando es false
+
+console.log('operador not', !suscrito); // true
+
