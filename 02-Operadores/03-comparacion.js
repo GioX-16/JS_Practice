4c3feac5b5a7023697fe6180621706cfc9bc7360
Geovanny Sandino <sandinowork16@gmail.com>
2024-12-26 23:58:16 -0600

    Practice with comparison operators

--- 02-Operadores/03-comparacion.js
+++ 02-Operadores/03-comparacion.js
@@ -1,8 +1,8 @@
-let a = 5;
+let a = 2;
 console.log(a > 5);  // false
-console.log(a >= 5); // True
+console.log(a >= 5); // false   
 
 
 let b = 4;
-console.log(b < 4);
-console.log(b <= 4);
+console.log(b < 4);  // false 
+console.log(b <= 4);  // true
