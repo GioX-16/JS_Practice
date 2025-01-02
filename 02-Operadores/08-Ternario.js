142fa089dc6969fdba5dc8912d182768e71232cd
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-02 14:16:18 -0600

    modify 07 y 08 files from js

---
+++ 02-Operadores/08-Ternario.js
@@ -0,0 +1,14 @@
+
+// Operador Ternario cumple con la funcion de un if else pero de manera simple
+
+let edad = 20;
+let acceso = edad > 17 ? 'Acceso Permitido' : 'Acceso Denegado';
+console.log(acceso);
+
+// En este caso la variable edad es mayor a 17 por lo que el resultado sera 'Acceso Permitido'
+
+// Tenemos otro ejemplo:
+
+let altura = 1.90;
+let Entrar = altura > 1.80 ? 'Puede subir' : 'No puede subir';
+console.log(Entrar);
