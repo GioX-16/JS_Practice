969fe53ef36a759aacb5895358c1b237e6bd0078
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-03 00:28:42 -0600

    Aprendimos a usar el bucle while en JavaScript

---
+++ 03-Control_flujo/03-while.js
@@ -0,0 +1,11 @@
+// Cuales son los numeros pares
+
+
+let i = 0;
+while (i < 10){
+    if (i % 2 == 0){
+        console.log('Este Numero es par', i);
+    }
+    i++;
+} 
+console.log('Fin del ciclo while');
