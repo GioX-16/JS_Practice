1065b3adb580186120a82c1481ca121ba8c0f559
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-04 13:57:54 -0600

    Learn To create a Infinit Loop

--- 03-Control_flujo/03-while.js
+++ 03-Control_flujo/03-while.js
@@ -4,7 +4,7 @@
 let i = 0;
 while (i < 20){
     if (i % 2 == 0){
-        console.log('El siguiente Numero', i , 'Es par');
+        console.log('El siguiente Numero es ', i, 'Es par');
     }
     i++;
 } 
---
+++ 03-Control_flujo/04-loop-infinito.js
@@ -0,0 +1,5 @@
+let i = 0;
+while (i < 20){
+    console.log(i);
+    i++; // Si se comenta esta linea se creara un loop infinito
+} 
