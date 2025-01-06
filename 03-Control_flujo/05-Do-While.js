3a5b9ae5f63a2ae0f232cdeb9473d937bc89d7b7
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-05 19:16:48 -0600

    Learn about Do While

---
+++ 03-Control_flujo/05-Do-While.js
@@ -0,0 +1,19 @@
+// Cuales son los numeros pares del 1 al 10
+
+
+let i = 0;
+/* while (i <  3) {
+    if (i % 2 == 0){
+        console.log('Este es un numero par', i);
+    }
+    i++;
+} */
+
+// Do-While
+do {
+    if (i % 2 == 0){
+        console.log('Este es un numero par', i);
+    }
+    i++;
+} while (i < 10); 
+
