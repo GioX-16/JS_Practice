8ff81741795229d8f19ff6abe5924acba5256e14
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-06 08:23:21 -0600

    Learn in js for of loop and how to use it

---
+++ 03-Control_flujo/07-for-of.js
@@ -0,0 +1,13 @@
+// While
+// do while
+// for
+
+// for of
+
+let nombres = ['Juan', 'Pedro', 'Maria', 'Ana'];
+
+for (let nombre of nombres){
+    console.log(nombre);
+}
+
+// En el For of podemos tomar un listado de nombres y recorrerlo de manera mas sencilla.
