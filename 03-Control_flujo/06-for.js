cb6331751e939fff758ba596bc289522b650ccf7
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-06 08:20:10 -0600

    Aprendimos sobre el control de flujo FOR

---
+++ 03-Control_flujo/06-for.js
@@ -0,0 +1,13 @@
+/* let i = 2;
+while (i < 2){
+    if(i % 2 == 0){
+        console.log('Este es un numero par', i);
+    }
+    i++;
+}  */
+
+for (let i = 0; i < 10; i++){
+    if (i % 2 == 0){
+        console.log('Este es un numero par', i);
+    }
+}
