22a03e60498f5ba347191ae2a1e2aaf23148ff60
Geovanny Sandino <sandinowork16@gmail.com>
2025-01-07 22:19:42 -0600

    Modifique un archivo en 02-Operadores de comparacion

--- 02-Operadores/03-comparacion.js
+++ 02-Operadores/03-comparacion.js
@@ -6,3 +6,10 @@ console.log(a >= 5); // false
 let b = 3;
 console.log(b < 4);  // false 
 console.log(b <= 4);  // true
+
+// otros ejemplos de comparacion
+
+let c = 5;
+console.log(c == 5);  // true
+console.log(c === 5);  // true
+console.log(c === '5');  // false
